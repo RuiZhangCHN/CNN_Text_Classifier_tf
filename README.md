dd3106ecb3bab5c2609d36fff837b98aee21e9a3
muenn <532592399@qq.com>
2019-04-04 16:45:10 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # CNN_Text_Classifier_tf
 基于CNN的文本分类器
+
+> A simple CNN-based Text Classifier Implementation with Tensorflow v1.13+, which achieve about 0.973 accuracy rate on yelp review dataset.
+> 
+> 一个简单的基于Tensorflow v1.13+实现的CNN文本分类器模型。该模型在Yelp评论数据集上的分类准确率达到约0.973。
+
+### 1. 模型使用
+#### i) 训练模型
+首先根据需要修改main.py文件中flags参数设置。将mode修改为train，运行：
+
+    python main.py
+    
+#### ii) 测试模型
+修改mode为test，运行：
+
+    python main,py
+
+#### iii) 使用模型
+需要自行实现solver.Solver.run()函数，注意修改batch_size=1。
+
+### 2. 主要函数说明
+
+#### i) prepro
+ - **create_vocabulary:** 生成word2idx和idx2word。（可针对自己的数据集仿写该代码）
+ - **create_yelp_ids:** 读取yelp数据文件生成ids文件。（可针对自己的数据集仿写该代码）
+
+#### ii) model.BiLSTM:
+ - **build_model:** 构建具有带训练参数的模型节点。
+ - **build_graph:** 构建计算图。
+ 
+#### iii) solver.Solver:
+ - **load_data:** 从ids文件中读取数据
+ - **prepare_text_batch:** 将长短不一的输入文本padding为相同长度的输入。
+ - **train:** 使用train数据集训练模型，根据模型在dev集上的表现保存最优模型。
+ - **test:** 测试模型在test数据集上的准确率。
+ - **run:** \[Not Implemented\] 未实现的方法，用于使用训练好的模型进行预测。
+ 
+### 3. 模型对比
+
+| Model | Accuracy(Yelp) | Code |
+| --- | ---| --- |
+| BiLSTM | 0.97292| [github](https://github.com/muenn/BiLSTM_Text_Classifier_tf) |
+| **CNN** | 0.97285 | [github](https://github.com/muenn/CNN_Text_Classifier_tf) |
---
+++ model.py
@@ -0,0 +1,104 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2019/4/4 10:18 AM.
+
+Author: Ruizhang1993 (zhang1rui4@foxmail.com)
+"""
+import tensorflow as tf
+
+class BiLSTM(object):
+
+    def __init__(self,
+                 vocab_size,
+                 batch_size,
+                 embedding_size,
+                 num_hidden_size,
+                 maxlen,
+                 num_categories=2):
+
+        self.vocab_size = vocab_size
+        self.batch_size = batch_size
+        self.embedding_size = embedding_size
+        self.num_hidden_size = num_hidden_size
+        self.maxlen = maxlen
+        self.num_categories = num_categories
+
+        self.filter_sizes = [1, 2, 3, 4, 5]
+        self.filter_nums = [10, 20, 20, 20, 20]
+
+        self._build_model()
+        self._build_graph()
+
+    def _build_model(self):
+
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        with tf.device('/cpu:0'):
+            with tf.variable_scope('embeddings', reuse=tf.AUTO_REUSE):
+                self.embeddings = tf.get_variable('embedding_lookup',
+                                                  [self.vocab_size, self.embedding_size],
+                                                  dtype=tf.float32)
+
+        self.hidden_proj = tf.layers.Dense(self.num_hidden_size, activation='linear')
+
+        def distinguish(input_x):
+            input_x_emb = self.hidden_proj(tf.nn.embedding_lookup(self.embeddings, input_x))
+            input_x_emb_expand = tf.expand_dims(input_x_emb, -1)
+
+            with tf.variable_scope('discriminator'):
+                pooled_outputs = []
+                for filter_size, num_filter in zip(self.filter_sizes, self.filter_nums):
+                    with tf.variable_scope('conv_maxpool-%s' % filter_size):
+                        filter_shape = [filter_size, self.num_hidden_size, 1, num_filter]
+                        W = tf.get_variable(name='W',
+                                            shape=filter_shape,
+                                            initializer=tf.initializers.truncated_normal(stddev=0.1))
+                        conv = tf.nn.conv2d(input_x_emb_expand,
+                                            W,
+                                            [1, 1, 1, 1],
+                                            padding='VALID',
+                                            name='conv')
+                        h = tf.nn.relu(conv)
+                        pooled = tf.nn.max_pool(h,
+                                                [1, self.maxlen - filter_size + 1, 1, 1],
+                                                [1, 1, 1, 1],
+                                                padding='VALID',
+                                                name='pool')
+                        pooled_outputs.append(pooled)
+                num_filter_total = sum(self.filter_nums)
+                h_pool = tf.concat(pooled_outputs, 3)
+                h_pool_flat = tf.reshape(h_pool, [-1, num_filter_total])
+                h_pool_flat = tf.contrib.layers.dropout(h_pool_flat, keep_prob=self.keep_prob)
+
+                with tf.name_scope('projection'):
+                    dis_dense = tf.layers.dense(h_pool_flat, self.num_hidden_size, name='dis_dense')
+                    dis_dense = tf.nn.tanh(dis_dense)
+                    dis_logits = tf.layers.dense(dis_dense, 2, name='dis_proj')
+
+            return dis_logits
+
+        self.distinguish = distinguish
+
+    def _build_graph(self):
+
+        self.texts = tf.placeholder(tf.int32, [None, self.maxlen], name='input_texts')
+        self.labels = tf.placeholder(tf.int64, [None], name='input_labels')
+
+        self.output = self.distinguish(self.texts)
+
+        self.ypred_for_auc = tf.nn.softmax(self.output)
+
+        self.loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=self.output+1e-10,
+                labels=self.labels
+            )
+        )
+
+        ypred = tf.cast(tf.argmax(self.output, 1), tf.int64)
+        correct = tf.equal(ypred, self.labels)
+        self.accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+        tvars = tf.trainable_variables()
+        opt = tf.train.AdamOptimizer()
+        self.train_op = opt.minimize(self.loss, var_list=tvars)
